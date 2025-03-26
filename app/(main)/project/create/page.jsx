20de426858b982ee58922124bcc5490aef72fdd4
Vinay Kumar <vinaydagar514@gmail.com>
2025-03-26 13:48:45 +0530

    Implement project creation form with validation; integrate react-hook-form and zod for input handling

--- app/(main)/project/create/page.jsx
+++ app/(main)/project/create/page.jsx
@@ -2,11 +2,25 @@
 import OrgSwitcher from "@/components/org-switcher";
 import { useOrganization, useUser } from "@clerk/nextjs";
 import React, { useEffect, useState } from "react";
+import { useForm } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { projectSchema } from "@/app/lib/validators";
+import { Input } from "@/components/ui/input";
+import { Textarea } from "@/components/ui/textarea";
+import { Button } from "@/components/ui/button";
 
 const CreateProjectPage = () => {
   const { isLoaded: isOrgLoaded, membership } = useOrganization();
   const { isLoaded: isUserLoaded } = useUser();
   const [isAdmin, setIsAdmin] = useState(false);
+
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm({
+    resolver: zodResolver(projectSchema),
+  });
 
   useEffect(() => {
     if (isOrgLoaded && isUserLoaded && membership) {
@@ -16,6 +30,8 @@ const CreateProjectPage = () => {
   if (!isOrgLoaded || !isUserLoaded) {
     return null;
   }
+
+  const onSubmit = async () => {};
   if (!isAdmin) {
     return (
       <div className="flex flex-col gap-2 items-center">
@@ -27,7 +43,56 @@ const CreateProjectPage = () => {
     );
   }
 
-  return <div>Create Project</div>;
+  return (
+    <div className="container mx-auto py-10">
+      <h1 className="text-6xl text-center font-bold mb-8 gradient-title">
+        Create New Project
+      </h1>
+      <form
+        className="flex flex-col space-y-4"
+        onSubmit={handleSubmit(onSubmit)}
+      >
+        <div>
+          <Input
+            id="name"
+            className="bg-slate-950"
+            placeholder="Project Name"
+            {...register("name")}
+          />
+          {errors.name && (
+            <p className="text-red-500 text-sm mt-1">{errors.name.message}</p>
+          )}
+        </div>
+        <div>
+          <Input
+            id="key"
+            className="bg-slate-950"
+            placeholder="Project Name"
+            {...register("key")}
+          />
+          {errors.key && (
+            <p className="text-red-500 text-sm mt-1">{errors.key.message}</p>
+          )}
+        </div>
+        <div>
+          <Textarea
+            id="key"
+            className="bg-slate-950"
+            placeholder="Project Name"
+            {...register("description")}
+          />
+          {errors.description && (
+            <p className="text-red-500 text-sm mt-1">
+              {errors.description.message}
+            </p>
+          )}
+        </div>
+        <Button type="submit" size="lg" className="bg-blue-500 text-white">
+          Create Project
+        </Button>
+      </form>
+    </div>
+  );
 };
 
 export default CreateProjectPage;
